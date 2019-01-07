1619f7a3ff3519b4ef6717c8a84370e351916549
Jemima Abu <jemimaabu@gmail.com>
2019-01-07 16:01:52 +0100

    Added methods for adding and displaying food choices

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 var foodChoices = ["Pancakes", "Fried rice", "Bread and egg", "Cereal", "Indomie", "Beans", "PB&J sandwiches", "Rice and stew"]
+var foodChoiceInput = document.getElementById("user-food-choice");
+var displayFoodChoice = document.getElementById("display-food-choice");
+var errorMessage = document.getElementById("form-error");
+
+function updateFoodChoices() {
+    displayFoodChoice.innerHTML = foodChoices.map(a => a)
+}
+updateFoodChoices();
 
 function generateRandomFood() {
     var randomIndex = Math.floor(Math.random()*foodChoices.length);
     document.getElementById("food-choice").innerHTML = foodChoices[randomIndex]
+}
+
+function addFoodChoice() {
+    if (foodChoiceInput.value === "") {
+        errorMessage.innerHTML = "Input cannot be empty"
+    } else if (foodChoices.includes(foodChoiceInput.value)) {
+        errorMessage.innerHTML = "Cannot include duplicate item"
+    } else {
+        errorMessage.innerHTML = "";
+        foodChoices.push(foodChoiceInput.value);
+        foodChoiceInput.value="";
+        updateFoodChoices();
+    }
 }
