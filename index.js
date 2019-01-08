a4935412fb869dddc24d88e6d671e34cd4e550dc
Jemima Abu <jemimaabu@gmail.com>
2019-01-08 22:50:05 +0100

    More JavaScript updates

--- index.js
+++ index.js
@@ -1,27 +1,50 @@
-var foodChoices = ["Pancakes", "Fried rice", "Bread and egg", "Cereal", "Indomie", "Beans", "PB&J sandwiches", "Rice and stew"]
 var foodChoiceInput = document.getElementById("user-food-choice");
 var displayFoodChoice = document.getElementById("display-food-choice");
 var errorMessage = document.getElementById("form-error");
 
-function updateFoodChoices() {
-    displayFoodChoice.innerHTML = foodChoices.map((food,i) => `<span id="foodchoice-${i}">${food}</span>`)
-}
-updateFoodChoices();
+//Array containing inappropriate swear words because isn't vulgarity just hilarious
+var inappropriate = ["fuck","ass","booty","dick","pussy","cock","penis","vagina","cunt","butt"]
 
-function generateRandomFood() {
-    var randomIndex = Math.floor(Math.random()*foodChoices.length);
-    document.getElementById("food-choice").innerHTML = foodChoices[randomIndex]
+//Array containing initial food choices
+var foodChoices = ["Pancakes", "Fried rice", "Bread and egg", "Cereal", "Indomie", "Beans", "PB&J sandwiches", "Rice and stew"]
+
+// Function to display added food choices
+function updateFoodChoices(food) {
+    var span = document.createElement("span")
+    span.innerHTML = food;
+    var click = document.createAttribute("onclick");
+    click.value = "deleteFoodChoice(this)";
+    span.setAttributeNode(click);
+    displayFoodChoice.appendChild(span);
 }
+
+// Display the choices in initial food choices 
+foodChoices.map(food => updateFoodChoices(food))
 
 function addFoodChoice() {
     if (foodChoiceInput.value === "") {
         errorMessage.innerHTML = "Input cannot be empty"
     } else if (foodChoices.includes(foodChoiceInput.value)) {
         errorMessage.innerHTML = "Cannot include duplicate item"
+    } else if (inappropriate.includes(foodChoiceInput.value.toLowerCase())) {
+        errorMessage.innerHTML = "Haha, very funny."
     } else {
-        errorMessage.innerHTML = "";
+        errorMessage.innerHTML = "";;
+        updateFoodChoices(foodChoiceInput.value);
         foodChoices.push(foodChoiceInput.value);
         foodChoiceInput.value="";
-        updateFoodChoices();
     }
+}
+
+function deleteFoodChoice(e) {
+    var index = foodChoices.indexOf(e.innerText);
+    if (index >= 0) {
+        foodChoices.splice(index, 1);
+        displayFoodChoice.removeChild(displayFoodChoice.children[index]);
+    }
+}
+
+function generateRandomFood() {
+    var randomIndex = Math.floor(Math.random()*foodChoices.length);
+    document.getElementById("food-choice").innerHTML = foodChoices[randomIndex]
 }
