c5a1518eaa8ae3168e53f0991292f4d8c50cf6c6
Jemima Abu <jemimaabu@gmail.com>
2019-01-11 13:45:07 +0100

    Added more text

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var foodChoiceInput = document.getElementById("user-food-choice");
 var displayFoodChoice = document.getElementById("display-food-choice");
 var errorMessage = document.getElementById("form-error");
-var foodChosen = document.getElementById("food-choice")
+var foodChosen = document.getElementById("food-choice");
+
+//Variable to detect how many times a user has generated a random food choice
+var foodCount = 0;
 
 //Array containing initial food choices
-var foodChoices = ["Pancakes", "Fried rice", "Bread and egg", "Cereal", "Indomie", "Beans", "PB&J sandwiches", "Rice and stew"]
+var foodChoices = ["Yam and egg", "Jollof rice", "Bread and egg", "Cereal", "Indomie", "Beans", "Efo riro", "Ofada rice and stew"]
 
 // Function to display added food choices
 function updateFoodChoices(food) {
@@ -26,12 +29,14 @@ function addFoodChoice() {
         errorMessage.innerHTML = "Cannot include duplicate item"
     } else {
         errorMessage.innerHTML = "";
+        foodCount = 0;
         updateFoodChoices(foodChoiceInput.value);
         foodChoices.push(foodChoiceInput.value);
         foodChoiceInput.value="";
     }
 }
 
+// Allow food choice to be added if Enter key is pressed in the input
 foodChoiceInput.onkeypress = function(event){
     if (event.which == 13 || event.keyCode == 13) {
         addFoodChoice()
@@ -54,10 +59,17 @@ function generateRandomFood() {
     } else if (foodChoices.length === 1) {
         errorMessage.innerHTML = "It's not very random if you only have one option"
     } else {
-        errorMessage.innerHTML = "";
-        var randomIndex = Math.floor(Math.random()*foodChoices.length);
-        foodChosen.innerHTML = foodChoices[randomIndex];
-        renderSuggestedLinks();
+        foodCount++;
+        // Condition to ensure user can only generate random food once
+        if (foodCount <= 1) {
+            errorMessage.innerHTML = "";
+            var randomIndex = Math.floor(Math.random()*foodChoices.length);
+            foodChosen.innerHTML = foodChoices[randomIndex];
+            renderSuggestedLinks();
+        } else {
+            errorMessage.innerHTML = "Sorry, no takebacks. It wouldn't be very random if you could just keep clicking. Enjoy your meal!"
+        }
+
     }
 }
 
